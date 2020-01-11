cdf532709f38f9d0c24d62e197ac4f9f17a6f4eb
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-11 18:34:37 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://messengerapp-env.us56ewjdta.us-east-1.elasticbeanstalk.com'
   //For H2-DB
   //apiUrl: 'http://todorestapi-env.rpzpu9tqns.us-east-1.elasticbeanstalk.com'
   //My SQL integrated Beanstalk managed
